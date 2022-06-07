c866e61947380000e7d3edd02c00d8bd0d528e4c
chris-one <102882070+chris-one@users.noreply.github.com>
2022-06-06 20:23:56 -0700

    added comments

--- Develop/script.js
+++ Develop/script.js
@@ -26,55 +26,45 @@ function generatePassword() {
   let temp = ''; 
   var passwordLength = prompt('You must choose password length. Minimum 8 characters, no more than 128 characters');
 
-
+  // min and max requirements for the password
   while (passwordLength < 8 || passwordLength > 128) {
     alert("Password must be between 8-128 characters"); 
+      // var length = false;
+      // alert("Invalid Password length");   
   }
   
   // options for characters numbers and symbol for the password
-  var lowerCase = confirm('Would you like lower case letters?');
-  var upperCase = confirm('Would you like upper case letters?');
-  var digits = confirm('Would you like numbers?');
-  var signs = confirm('Would you like symbols?');
+  var lowerCase = confirm('Would you like LOWERCASE letters?');
+  var upperCase = confirm('Would you like UPPERCASE letters?');
+  var digits = confirm('Would you like NUMBERS?');
+  var signs = confirm('Would you like SYMBOLS?');
   
-  // the input options
+  // input options lowercase
   if (lowerCase){
     temp += lowerCaseLetters;
   }
-
+  // input for uppercase letters
   if (upperCase){
     temp += upperCaseLetters;
   }
-
+  // input option for numbers 
   if (digits){
     temp += numbers;
   }
-
+  // input options for symbols
   if (signs){
     temp += symbols
   }
-
+  // the alert if none of the options were inputed
   if (!lowerCase && !upperCase && !digits && !signs){
     alert('Please choose atleast one!'); 
   }
-
+  // password generator
   for (i = 0; i < passwordLength; i++){
     let randomNumber = Math.floor(Math.random() * temp.length);
     finalPassword += temp.charAt(randomNumber);
   }
-
+  // password given
   return finalPassword;
-} 
-
   
-
-
-
-
-
-
-
-
-
-
-
+}
